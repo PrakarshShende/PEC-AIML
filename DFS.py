f65b5241c6908c920af3f4f74bf662036613c3bf
Prakarsh <prakarshshende2901@gmail.com>
2026-08-17 14:28:07 +0530

    updated DFS code

--- DFS.py
+++ DFS.py
@@ -1,46 +1,21 @@
-def dfs(graph, start, goal):
-
-    OPEN = [start]
-    CLOSED = []
-    traversal = []
-
-    while OPEN:
-
-        x = OPEN.pop(0)
-
-        # Add node to traversal
-        traversal.append(x)
-
-        # Check if goal is reached
-        if x == goal:
-            print("DFS Traversal:", " → ".join(traversal))
-            print("Goal found:", x)
-            return
-
-        # Add current node to CLOSED
-        if x not in CLOSED:
-            CLOSED.append(x)
-
-            # Get children
-            children = graph[x]
-
-            # Add children to LEFT of OPEN
-            for child in reversed(children):
-
-                if child not in OPEN and child not in CLOSED:
-                    OPEN.insert(0, child)
-
-    print("Goal not found")
-
-
-# Tree
 graph = {
-    'A': ['B', 'C'],
-    'B': ['D', 'E'],
-    'C': [],
-    'D': [],
-    'E': []
+    '5': ['3', '7'],
+    '3': ['2', '4'],
+    '7': ['8'],
+    '2': [],
+    '4': ['8'],
+    '8': []
 }
 
-# DFS
-dfs(graph, 'A', 'E')
+visited = set()
+
+def dfs(visited, graph, node):
+    if node not in visited:
+        print(node)
+        visited.add(node)
+
+        for neighbour in graph[node]:
+            dfs(visited, graph, neighbour)
+
+print("Following is the Depth-First Search")
+dfs(visited, graph, '5')
